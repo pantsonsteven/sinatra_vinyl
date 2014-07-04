e2cd6e8fc20ba3f76d0b00f9d98d80d9a1f04d71
pantsonsteven <pantsonsteven@gmail.com>
2014-07-04 00:04:05 -0400

    creates edit, index, new, and show views; makes db connection with get request to "/records"

--- app.rb
+++ app.rb
@@ -1,2 +1,22 @@
 require 'bundler'
 Bundler.require
+
+get '/' do 
+   redirect '/records'
+end
+
+get '/records' do 
+
+   connection = PG.connect(dbname: 'vinyl')
+   sql_statement = 'SELECT * FROM records;'
+   response = connection.exec(sql_statement)
+
+   @records = response.map do |record|
+      {'title' => record['title'], 'artist' => record['artist']}
+   end
+
+   connection.close
+
+   erb :index
+
+end
